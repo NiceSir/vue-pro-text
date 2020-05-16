6e152060e7d72fa6c1228b237d4c46942f9b98d1
jiro <13676083987@163.com>
2020-05-16 19:55:20 +0800

    111

--- config/index.js
+++ config/index.js
@@ -37,14 +37,12 @@ module.exports = {
   },
 
   build: {
-    // // Template for index.html
-    // index: path.resolve(__dirname, '../dist/index.html'),
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
 
-    // // Paths
-    // assetsRoot: path.resolve(__dirname, '../dist'),
-    
-    index: path.resolve(__dirname, '../docs/index.html'),
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
